875c7c0ba2efcd85c759c71c3b186aedb4b8f5db
Mark Louie <dullavin.marklouie.d@gmail.com>
2024-03-20 14:56:25 +0800

    Initial Commit

--- app.js
+++ app.js
@@ -9,13 +9,6 @@ app.get("/", (req, res) => {
 });
 
 var animalsArray = ["Elephant", "Eagle", "Dragonfly"];
-const API_KEY = "ccCBd7oOPGXnF/Byo9+rUw==FrPn9rqGncHCgE4u";
-
-const groupedAnimals = {
-  Mammals: [],
-  Birds: [],
-  Insects: [],
-};
 
 // Can you categorize the animalsArray into the following categories?
 
@@ -25,29 +18,34 @@ const groupedAnimals = {
 //     Birds: [ 'Eagle' ],
 //     Insects: [ 'Dragonfly' ]
 // }
+//
+const categorizedAnimal = {
+  Mammals: [],
+  Birds: [],
+  Insects: [],
+};
 
-animalsArray.forEach((animal, index) => {
+animalsArray.map((animal, index) => {
   if (animal === "Elephant") {
-    groupedAnimals.Mammals.push(animal);
+    categorizedAnimal.Mammals.push(animal);
   } else if (animal === "Eagle") {
-    groupedAnimals.Birds.push(animal);
+    categorizedAnimal.Birds.push(animal);
   } else if (animal === "Dragonfly") {
-    groupedAnimals.Insects.push(animal);
+    categorizedAnimal.Insects.push(animal);
   }
 });
-
-// console.log(groupedAnimals);
-
-//
 
 // After you get the desired output create an API call to this API endpoint
 // https://api.api-ninjas.com/v1/animals
 // Or Visit this URL for more info: https://api-ninjas.com/api/animals
 // API-Key to use: ccCBd7oOPGXnF/Byo9+rUw==FrPn9rqGncHCgE4u
 
-const getAnimals = async (animalName) => {
-  const inputArray = Object.values(animalName);
-  inputArray.forEach(async (animal) => {
+const API_KEY = "ccCBd7oOPGXnF/Byo9+rUw==FrPn9rqGncHCgE4u";
+
+const getAnimals = async (groupedAnimals) => {
+  const inputAnimals = Object.values(groupedAnimals);
+
+  inputAnimals.forEach(async (animal, index) => {
     try {
       const data = await axios(
         `https://api.api-ninjas.com/v1/animals?name=${animal}`,
@@ -59,12 +57,13 @@ const getAnimals = async (animalName) => {
       );
       console.log(data.data);
     } catch (error) {
-      console.log(error);
+      console.log(error.message || error);
+      throw new Error(error.message || error);
     }
   });
 };
 
-getAnimals(groupedAnimals);
+getAnimals(categorizedAnimal);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT:${PORT}`);
